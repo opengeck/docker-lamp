86c8dcf7f824495e7bfefc8a05fdf7cfc89432e2
noxhalt <hlt@tuta.io>
2020-03-04 10:34:11 +0900

    [remove] dangerous script.

--- reset.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$(readlink -f "$0")")" || exit
-
-echo 'Reset docker container.'
-docker-compose down
-sudo rm -r ./srv/mysql ./srv/phpmyadmin 2>/dev/null
-docker-compose build \
-  && docker-compose up -d --remove-orphans \
-  && echo 'done'
